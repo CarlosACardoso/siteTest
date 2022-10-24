b15aa997ca57648ed33ceb1ac00f0dc608b42db5
Carlos Alberto Cardoso <carloalbertocardoso@gmail.com>
2022-10-24 13:55:04 -0300

    first commit

---
+++ app/routes.py
@@ -0,0 +1,21 @@
+from app import app
+from flask import render_template
+from flask import request
+from pymongo import MongoClient
+
+client = MongoClient("mongodb+srv://adminuser:adminuser@mygame.t19zp9a.mongodb.net/?retryWrites=true&w=majority")
+db = client.get_database("clientslandpage") #Trocar caso mude a database
+client = db.clients_landpage #Mudar caso mude a tabela
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('login.html')
+
+@app.route('/salvar', methods=['GET'])
+def salvar():
+    usuario = request.args.get('usuario')
+    senha = request.args.get('senha')
+    data = {"nome":usuario,"senha:":senha}
+    client.insert_one(data)
+    return "Logo entraremos em contato!"
